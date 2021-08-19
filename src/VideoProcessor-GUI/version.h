ab9ed8c03e69b78c315670703e62d25ff1dd99eb
Dennis Fleurbaaij <mail@dennisfleurbaaij.com>
2021-08-19 21:12:33 +0200

    fixed version generation

--- src/VideoProcessor-GUI/version.h
+++ src/VideoProcessor-GUI/version.h
@@ -2,9 +2,9 @@
 
 #include <atlstr.h>
 
-static const TCHAR* VERSION_URL=TEXT("https://github.com/defl/videoprocessor/commit/42b817d4e3733333c19ee8fd93804f1431d8ec5b");
-static const TCHAR* VERSION_DATE=TEXT("2021-07-27 19:47:43 +0200");
-static const TCHAR* VERSION_DESCRIBE=TEXT("v0.4.2_1-0-g42b817d");
+static const TCHAR* VERSION_URL=TEXT("https://github.com/defl/videoprocessor/commit/6047f217154148100fc397160461b68e2c1039a5");
+static const TCHAR* VERSION_DATE=TEXT("2021-08-19 21:10:07 +0200");
+static const TCHAR* VERSION_DESCRIBE=TEXT("v0.4.2_1-6-g6047f21");
 static const TCHAR* VERSION_AUTHOR=TEXT("Dennis Fleurbaaij <mail@dennisfleurbaaij.com>");
 const bool VERSION_DIRTY=true;
 
